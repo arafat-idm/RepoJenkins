7a55a238fc27080c3ffaaa7796d14be96200ef0c
arafat-dm <arafat.espinosa@idmdev.tech>
2023-01-12 16:11:43 -0600

    Se agrega clase
    Se agrega prueba para git y jenkins

---
+++ HolaMundo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    static class HolaMundo
+    {
+        public static string Hola_Mundo = "Hola Mundo Desde Una Clase Nueva";
+    }
+}
