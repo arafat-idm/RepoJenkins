4467cb74321edab6c6df03d5c40a5fb39976f97b
arafat-dm <arafat.espinosa@idmdev.tech>
2023-01-12 16:19:04 -0600

    PRUEBA

--- HolaMundo.cs
+++ HolaMundo.cs
@@ -6,6 +6,6 @@ namespace CSharp
 {
     static class HolaMundo
     {
-        public static string Hola_Mundo = "Hola Mundo Desde Una Clase Nueva prueba 3";
+        public static string Hola_Mundo = "Hola Mundo Desde Una Clase Nueva";
     }
 }
